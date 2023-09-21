a5d77153ea0ac838b33dfe0c101b9ffa4e5298d1
Dyogo Henrique <105476232+dyogohenrique@users.noreply.github.com>
2023-09-21 18:36:13 -0400

    Mais

--- src/components/form/Button.jsx
+++ src/components/form/Button.jsx
@@ -3,7 +3,11 @@ import styles from './Button.module.css';
 
 /**
  * Cria um botão de submit
- * @param text Texto que aparece dentro do botão 
+ * @param text Texto que aparece dentro do botão
+ * @param name Name e id do botão
+ * @param type Tipo do  botão
+ * @param handleOnchange Função para o onClick
+ * @param customClass Classe personalizada
  * @returns retorna o botão de submit
  */
 function Button({ text, name, type, handleOnchange,customClass }) {
